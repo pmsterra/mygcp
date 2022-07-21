b5b2ae13669bad5be0e87b762b2be137a27710d3
pmsterra <pms007@gmail.com>
2022-07-21 22:17:35 +0530

    initial

--- provider.tf
+++ provider.tf
@@ -1,8 +1,6 @@
 provider "google" {
   version = ">= 3.69"
 
-  #credentials = "/c/Users/pms00/OneDrive/Documents/terraform/my-test-1-355710-7dd15c0e66d6.json"
-  #credentials = "${file("/c/Users/pms00/terraform/")}"
 
   #
   region  = "us-central1"
