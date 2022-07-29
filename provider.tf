6ce47ba3d675210160aaaa2d89e4fb8a0391bb45
pmsterra <pms007@gmail.com>
2022-07-29 17:16:37 +0530

    changes

--- provider.tf
+++ provider.tf
@@ -2,16 +2,16 @@ provider "google" {
   version = ">= 3.69"
 
 
-  #
+  
   region  = "us-central1"
 }
 
 
-# provider "google-beta" {
-#   version = ">= 3.69"
-#   #provider = google-beta
+ provider "google-beta" {
+   version = ">= 3.69"
+  #provider = google-beta
 
-#   #
-#   region  = "us-central1"
-# }
+
+  region  = "us-central1"
+}
 
